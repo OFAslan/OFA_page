f8b681ea625f6d74ed5c8ea9a39743bdf067ed70
Oguzhan Aslan <106531243+OFAslan@users.noreply.github.com>
2024-04-08 22:39:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from PIL import Image
 
 load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
 
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('photo.png'))
@@ -16,5 +15,5 @@ st.info('Data & Analytics Specialist, Data Engineer')
 icon_size = 23
 
 st_button('medium', 'https://medium.com/@furkan-aslan', 'Read my Blogs', icon_size) 
-st_button('linkedin', 'https://www.linkedin.com/in/oguzhanaslan3707/', 'Connect with Me on LinkedIn', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/oguzhanaslan3707/', 'Connect with me on LinkedIn', icon_size)
 st_button('github', 'https://github.com/OFAslan', 'My Github Link', icon_size)
