b2319cd5853ba497f84fdbb1f5441570b2a47a1a
Oguzhan Aslan <106531243+OFAslan@users.noreply.github.com>
2024-04-08 22:48:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ load_css()
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('photo.png'))
 
-col2.write("OGUZHAN FURKAN ASLAN", style={"font-size": "40px"})
+col2.header("OGUZHAN FURKAN ASLAN", style={"font-size": "40px"})
 
 st.info('Data & Analytics Specialist, Data Engineer')
 
